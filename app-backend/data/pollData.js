f3863cff4ef26a7718eccd14277d3a8a8af78201
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2024-02-16 19:40:46 +0000

    Created poll data

---
+++ app-backend/data/pollData.js
@@ -0,0 +1,60 @@
+const pollData = [
+    {
+      "pollId": 1,
+      "pollName": "Premier League Winner",
+      "question": "Who will win the Premier League?",
+      "options": [
+        {
+          "optionId": 1,
+          "optionText": "Manchester City"
+        },
+        {
+          "optionId": 2,
+          "optionText": "Arsenal"
+        },
+        {
+          "optionId": 3,
+          "optionText": "Liverpool"
+        }
+      ]
+    },
+    {
+      "pollId": 2,
+      "pollName": "Upcoming Football Matches",
+      "question": "Which team will win the upcoming football match?",
+      "options": [
+        {
+          "optionId": 1,
+          "optionText": "Manchester United"
+        },
+        {
+          "optionId": 2,
+          "optionText": "Chelsea"
+        },
+        {
+          "optionId": 3,
+          "optionText": "Real Madrid"
+        }
+      ]
+    },
+    {
+      "pollId": 3,
+      "pollName": "Champions League Predictions",
+      "question": "Who will win the Champions League this season?",
+      "options": [
+        {
+          "optionId": 1,
+          "optionText": "Barcelona"
+        },
+        {
+          "optionId": 2,
+          "optionText": "Bayern Munich"
+        },
+        {
+          "optionId": 3,
+          "optionText": "Paris Saint-Germain"
+        }
+      ]
+    }
+  ];
+  
