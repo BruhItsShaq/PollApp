a253e0ee20f68d3cdaa3ec99f79d9106283269f1
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2024-02-16 21:51:02 +0000

    removed additional poll data

--- app-backend/data/pollData.js
+++ app-backend/data/pollData.js
@@ -17,44 +17,6 @@ const pollData = [
           "optionText": "Liverpool"
         }
       ]
-    },
-    {
-      "pollId": 2,
-      "pollName": "Upcoming Football Matches",
-      "question": "Which team will win the upcoming football match?",
-      "options": [
-        {
-          "optionId": 1,
-          "optionText": "Manchester United"
-        },
-        {
-          "optionId": 2,
-          "optionText": "Chelsea"
-        },
-        {
-          "optionId": 3,
-          "optionText": "Real Madrid"
-        }
-      ]
-    },
-    {
-      "pollId": 3,
-      "pollName": "Champions League Predictions",
-      "question": "Who will win the Champions League this season?",
-      "options": [
-        {
-          "optionId": 1,
-          "optionText": "Barcelona"
-        },
-        {
-          "optionId": 2,
-          "optionText": "Bayern Munich"
-        },
-        {
-          "optionId": 3,
-          "optionText": "Paris Saint-Germain"
-        }
-      ]
     }
   ]
-  module.exports = pollData;
+module.exports = pollData;
