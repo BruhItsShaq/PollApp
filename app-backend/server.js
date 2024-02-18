3cd8fe8a0d0a3f0eece13bb74daeb1f677f175ce
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2024-02-18 04:28:46 +0000

    Installed CORS

--- app-backend/server.js
+++ app-backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const router = require('./router'); // Import router module
 const PORT = process.env.PORT || 3000;
 
-
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
