d9e2a6a04a75eac8468d440ae1e96fa3665ec564
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2024-02-19 14:13:07 +0000

    Added Routing in to switch pages

--- app-frontend/votingapp/src/App.js
+++ app-frontend/votingapp/src/App.js
@@ -1,10 +1,22 @@
-import VotingPage from ".//components/VotingPage";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import VotingPage from "./components/VotingPage";
+import ConfirmationPage from "./components/ConfirmationPage";
 
 function App() {
+  const pollId = 1; //pollId set to be passed as a prop
   return (
-    <div className="App">
-      <VotingPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <VotingPage pollId={pollId} />
+          </Route>
+          <Route path="/confirmation">
+            <ConfirmationPage pollId={pollId} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
