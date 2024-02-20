ffc9f0963a5d5358ce36c21927771238df628b0a
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2024-02-20 04:47:54 +0000

    Fixed mapping errors and added function for options text

--- app-frontend/votingapp/src/components/ConfirmationPage.js
+++ app-frontend/votingapp/src/components/ConfirmationPage.js
@@ -5,25 +5,39 @@ import logo from '../assets/assets/logo.png';
 import background from '../assets/assets/background.jpg';
 
 const ConfirmationPage = ({ pollId }) => { //Accept pollId as a prop
-    const [ votes, setVotes ] = useState([]);
+    const [votes, setVotes] = useState({});
 
     useEffect(() => {
         //Fetch votes from the server
         const fetchVoteData = async () => {
             try {
                 const voteData = await getVotes(pollId);
+                console.log('Vote data:', voteData);
                 setVotes(voteData);
             } catch (error) {
-                console.error('Error fetching votes:', error);
+                if (error.response && error.response.status === 404) {
+                    console.error('No votes found for this poll');
+                    // Handle case where there are no votes for the poll
+                } else {
+                    console.error('Error fetching votes:', error);
+                }
             }
         };
         fetchVoteData();
-    }, [pollId, setVotes]);
+    }, [pollId, votes]);
 
     //Function to calculate the % of the votes
-    const calculatePercentage = (votesCount) => {
-        const totalVotes = votes.reduce((total, votes) => total + votes.count, 0);
+    const calculatePercentage = (votesCount, totalVotes) => {
         return ((votesCount / totalVotes) * 100).toFixed(0);
+    };
+
+    const totalVotes = Object.values(votes).reduce((total, count) => total + count, 0);
+
+    //Sample option texts due to pollData out of src directory
+    const optionTexts = {
+        1: "Manchester City",
+        2: "Arsenal",
+        3: "Liverpool"
     };
 
     return (
@@ -32,9 +46,11 @@ const ConfirmationPage = ({ pollId }) => { //Accept pollId as a prop
             <div className="content">
                 <h1>Thank you for <b>your response</b></h1>
                 <div className="results">
-                    {votes.map((vote) => (
-                        <div key={vote.optionId}>
-                            <p>{vote.optionText} {calculatePercentage(vote.count)}%</p>
+                    {Object.entries(votes).map(([optionId, count]) => (
+                        <div className="outerProgress" >
+                            <div key={optionId} className="progress"style={{ width: `${calculatePercentage(count, totalVotes)}%` }}>
+                                <p>{optionTexts[optionId]} {calculatePercentage(count, totalVotes)}%</p>
+                            </div>
                         </div>
                     ))}
                 </div>
