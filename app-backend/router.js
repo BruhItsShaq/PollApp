ad51d2864f249a6aec0a8634a6fe266415ec4029
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2024-02-20 01:07:03 +0000

    Changed backend so that votes get assigned to each option

--- app-backend/router.js
+++ app-backend/router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const pollData = require('./data/pollData');
 const router = express.Router();
-let votes = []; //store submitted vote
+let votes = {}; //store submitted vote
 
 //Route for getting a poll by ID
 router.get('/polls/:id', (req, res) => {
     const { id } = req.params;
     const poll = pollData.find(poll => poll.pollId === parseInt(id));
-    if (!poll){
+    if (!poll) {
         return res.status(404).send('Poll not found');
     }
     res.json(poll);
@@ -15,47 +15,62 @@ router.get('/polls/:id', (req, res) => {
 
 //Route for submitting a vote
 router.post('/polls/:id/vote', (req, res) => {
-    const {id} = req.params;
-    const {optionId} = req.body;
+    const { id } = req.params;
+    const { optionId } = req.body;
     const poll = pollData.find(poll => poll.pollId === parseInt(id));
 
     //Checked to see if optionId is provided
-    if(!optionId){
+    if (!optionId) {
         return res.status(400).send('Please pick an option!');
     }
-    
+
     //Checks to see if poll exists
-    if (!poll){
+    if (!poll) {
         return res.status(404).send('Poll not found');
     }
 
     //Checks if the provided optionId exists within the poll's options
-    const optionExists = poll.options.some(option => option.optionId === parseInt(optionId));
+    const optionExists = poll.options.find(option => option.optionId === parseInt(optionId));
 
     if (!optionExists) {
         return res.status(400).send('Invalid optionId');
     }
 
-    //store vote in array with ID and option
-    votes.push({ pollId: parseInt(id), optionId: parseInt(optionId)});
+    //Check to see if current poll ID exists in vote object,
+    //if not then initialise an empty object
+    if (!votes[id]) {
+        votes[id] = {};
+    }
+
+    //Checks to see current optionID if it exists within the current pollID
+    //If not, initialise with value of 0
+    if (!votes[id][optionId]) {
+        votes[id][optionId] = 0;
+    }
+
+    //Increment vote for specified option within specified pollID
+    votes[id][optionId]++;
 
     res.send('Vote submitted successfully');
 });
 
 //Route for getting polls by their ID
 router.get('/polls/:id/votes', (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const poll = pollData.find(poll => poll.pollId === parseInt(id));
 
 
     //Checks to see if poll exists
-    if (!poll){
+    if (!poll) {
         return res.status(404).send('Poll not found');
     }
 
-    //Filters votes out using poll ID
-    const pollVotes = votes.filter(vote => vote.pollId === parseInt(id));
-
+    //Returns the votes for each option of poll
+    const votesForPoll = {};
+    poll.options.forEach(option => {
+        votesForPoll[option.optionId] = votes[id]?.[option.optionId] || 0;
+    });
+    
     res.json(pollVotes);
 });
 
